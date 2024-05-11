b53b9501d5c2550e161c1ee7a7caadb93e3cfee0
Kash-2g <162958950+Kash-2g@users.noreply.github.com>
2024-05-11 20:19:29 +0300

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -17,5 +17,18 @@ function App() {
     </div>
   );
 }
+const [pic, dog] = useState();
 
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        dog(data.message);
+      });
+  }, []);
+
+
+  if (!pic) return <p>Loading...</p>;
+
+  return <img src={pic} alt="A Random Dog" />;
 export default App;
